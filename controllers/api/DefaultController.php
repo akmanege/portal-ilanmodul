015dac7593c9d1d31a9faaa29bbfc9f4be3235a5
aegean11 <ege.akman1@gmail.com>
2019-01-06 15:33:29 +0300

    first commit

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\ilanmodul\controllers\api;
+
+
+/**
+ * Default controller for the `ilanmodul` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
---
+++ controllers/console/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\ilanmodul\controllers\console;
+
+
+/**
+ * Default controller for the `ilanmodul` module
+ */
+class DefaultController extends \kouosl\base\controllers\console\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
